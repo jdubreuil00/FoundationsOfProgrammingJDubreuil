5cb8001270602f8b30ac35118b3def5d045f9040
jdubreuil00 <dubreuilj@merrimack.edu>
2023-03-16 16:39:07 -0400

    Assignment Finished

--- A1/circle.py
+++ A1/circle.py
@@ -1,19 +1,22 @@
 import math
-from decimal import Decimal, getcontext
 
 #Accept user input for radius
 while True:
     try:
-        radius = float(input("Enter the radius:"))
+        radius = float(input('Enter the radius:'))
     except ValueError:
-        print("Sorry please try a valid number")
+        #Validate input
+        print('Sorry please try a valid number')
+        continue
+    if radius<0:
+        print('Please no negative numbers')
         continue
     else:
         break
     
 
 #Output radius to user
-print(radius)
+print(format(radius,'.3f'))
 #Circumference of circle based on Radius
 circumference = 2 * math.pi * radius
 #Area of circle based on radius
@@ -21,18 +24,8 @@ area = math.pi * radius**2
 #Volume of sphere based on radius
 volume = float(4/3)*(math.pi * radius**3)
 #Output circumference
-print("The circumference of a circle with radius " + str(radius) + " is {:.5f}".format(circumference) )
+print('The circumference of a circle with radius {:.3f}'.format(radius) + ' is {:.5f}'.format(circumference) )
 #Output area
-print("The area of a circle with a radius "+ str(radius) + " is {:.5f}".format(area))
+print('The area of a circle with a radius {:.3f}'.format(radius) + 'is {:.5f}'.format(area))
 #Output volume of sphere
-print("The volume of a sphere is with a radius "+ str(radius) + " is {:.5f}".format(volume))
-
-"""
-#Unedited output for testing
-getcontext().prec = 2
-print(float(Decimal(circumference)))
-print("circumference: " + str(circumference))
-print("area: " + str(area))
-print("volume: " + str(volume))
-"""
-
+print('The volume of a sphere is with a radius {:.3f}'.format(radius) + ' is {:.5f}'.format(volume))
