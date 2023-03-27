fa5b00ada29e1cc50bfc5dc5ec3b3b75b1187185
jdubreuil00 <dubreuilj@merrimack.edu>
2023-03-27 19:17:40 -0400

    Added the files for Assignment 2 and 3

--- A2/divider.py
+++ A2/divider.py
@@ -1,24 +1,31 @@
 import math
 
+# While loop validates user input
 while True:
     try:
+        # Program asks for two positive integers A and B
         a = int(input("Enter the first positive integer: "))
         b = int(input("Enter the second positive integer: "))
     except ValueError:
-        #Validate input
+        #Validates input with a ValueError continues to and contiues to ask for input
         print('Sorry please try a valid number')
         continue
     if a<0 or b<0:
+        #Checks to see if both inputs A and B are positive and continues to ask for input
         print('Please no negative numbers')
         continue
     else:
+        #Input is valid run the function to check if evenly divisible
         break
 
+# Returns if the two integers are divisible evenly
 def evalInt(a,b):
-    if a % b == 0 and b % a == 0:
-        print("True")
-    else:
-        print("False")
+    return a % b == 0 and b % a == 0
 
+# Calls the function and prints the returned resiult
+print(evalInt(a,b))
 
-evalInt(a,b)
+"""
+Was not sure if the program should continue after validation and evalInt returns false.
+From the wording of the assignment it seemed the progam only needed to ask for valid input.
+"""
---
+++ Week2ClassDocs/loopcountdown.py
@@ -0,0 +1,31 @@
+"""
+For the first fragment i is the incrementing value for the range 
+for 50 through 25. I extended the range to include 25 and incremented
+the loop using the third parameter in range for -5
+it then prints the values for 50 through 25
+"""
+#
+# for i in range(50,20,-5):
+#     print(i)
+
+"""
+For the second fragment I use the do-while loop to 
+promt the user to input a word and if they ould like to quit the 
+program to type the word EXIT. If you enter the word EXIT, the 
+condtion returns TRUE and escapes the while loop. If not
+the the input is printed and the user is prompted to type a word or EXIT.
+"""
+# while True:
+#     data = str(input('Type in a word or type EXIT to quit!'))
+#     if data == 'EXIT':
+#         print('Exiting the program.')
+#         break     
+#     else:
+#         print(data)
+
+def loop():
+    response = " "
+    while response != 'EXIT':
+        response = input("type input: ")
+        pass
+loop()
